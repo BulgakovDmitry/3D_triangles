b9a468de0e85fb995479793dab1b5bcdff0530b4
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-10-08 01:19:44 +0300

    fix cmake

---
+++ build-debug/tests/CMakeFiles/geometry.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geometry.dir/geometry_tests.cpp.o"
+  "CMakeFiles/geometry.dir/geometry_tests.cpp.o.d"
+  "geometry"
+  "geometry.pdb"
+  "geometry[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
