bfc9557f73e3eff3e1ee04c91de5b8f32e585eb1
GitHub Action <action@github.com>
2025-10-09 12:15:22 +0000

    Auto-format code

--- include/BVH/BVH.hpp
+++ include/BVH/BVH.hpp
@@ -25,7 +25,8 @@ const std::string dump_file_png = "../dump/graph_dump.png";
 enum class Axis { axis_x = 0, axis_y = 1, axis_z = 2 };
 
 template <std::floating_point T>
-inline bounding_box::AABB<T> calculate_bounding_box(const std::span<triangle::Triangle<T>> &triangles);
+inline bounding_box::AABB<T>
+calculate_bounding_box(const std::span<triangle::Triangle<T>> &triangles);
 
 /* ---------- Bounding Volume Hierarchy ---------- */
 template <std::floating_point T> class BVH {
@@ -256,7 +257,8 @@ void BVH<T>::dump_graph_connect_nodes(const std::unique_ptr<Node<T>> &node,
 }
 
 template <std::floating_point T>
-inline bounding_box::AABB<T> calculate_bounding_box(const std::span<triangle::Triangle<T>> &triangles) {
+inline bounding_box::AABB<T>
+calculate_bounding_box(const std::span<triangle::Triangle<T>> &triangles) {
     bounding_box::AABB<T> box;
     for (const auto &tr : triangles)
         box.wrap_in_box_with(tr.get_box());
