0c5b85c995abb0f63daea5d95fc7186753880ee0
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-10-25 18:19:51 +0300

    [graphics_driver, camera] add zoom - add callback for scrolling - add a handler for this event - add realisation of scrolling in camera (method process_mouse_scroll)

--- include/graphics/camera.hpp
+++ include/graphics/camera.hpp
@@ -13,27 +13,27 @@ namespace triangle {
 
 struct Camera {
     glm::vec3 position; // текущая позиция камеры
-    glm::vec3 world_up; // мировая ориентация
+    glm::vec3 world_up; // абсолютная ориентация 
 
-    /* ———————————————————————————— направляющие векторы и углы —————————————————————*/
+    /*———————————————————————————— направляющие векторы и углы —————————————————————*/
     glm::vec3 front;
     glm::vec3 up;
     glm::vec3 right;
     float yaw, pitch;
-    /* ——————————————————————————————————————————————————————————————————————————————*/
+    /*——————————————————————————————————————————————————————————————————————————————*/
 
-    /* —————————————————————————————— параметры движения ————————————————————————————*/
-    float movementSpeed;
-    float mouseSensitivity;
+    /*—————————————————————————————— параметры движения ————————————————————————————*/
+    float movement_zoom_speed;
+    float movement_speed;
+    float mouse_sensitivity;
     float zoom;
-    /* ——————————————————————————————————————————————————————————————————————————————*/
+    /*——————————————————————————————————————————————————————————————————————————————*/
 
     void update_camera();
 
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f),
            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0f, float pitch = 0.0f)
-        : front(glm::vec3(0.0f, 0.0f, -1.0f)), movementSpeed(2.5f), mouseSensitivity(0.1f),
-          zoom(45.0f) {
+        : front(glm::vec3(0.0f, 0.0f, -1.0f)), movement_zoom_speed(15.0f), movement_speed(2.5f), mouse_sensitivity(0.1f), zoom(30.0f) {
         this->position = position;
         this->world_up = up;
         this->yaw = yaw;
@@ -48,13 +48,15 @@ struct Camera {
         right,
         up,
         down,
+        zoom_in,
+        zoom_out,
     };
 
     glm::mat4 get_view_matrix() { return glm::lookAt(position, position + front, up); }
 
     void process_keyboard(Camera_movement direction, float delta_time);
     void process_mouse_movement(float xoffset, float yoffset, bool constrainPitch = true); // TODO
-    void process_mouse_scroll(float yoffset);                                              // TODO
+    void process_mouse_scroll(Camera_movement direction, float delta_time); 
 
     float get_zoom() const { return zoom; }
     float get_yaw() const { return yaw; }
@@ -78,7 +80,7 @@ void Camera::update_camera() {
 }
 
 void Camera::process_keyboard(Camera_movement direction, float delta_time) {
-    float velocity = movementSpeed * delta_time;
+    float velocity = movement_speed * delta_time;
 
     switch (direction) {
     case Camera_movement::forward: {
@@ -105,6 +107,24 @@ void Camera::process_keyboard(Camera_movement direction, float delta_time) {
         position -= up * velocity;
         break;
     }
+    }
+}
+
+void Camera::process_mouse_scroll(Camera_movement direction, float delta_time) {
+    float velocity = movement_zoom_speed * delta_time;
+
+    if (zoom < 1.0f) zoom = 1.0f;
+    if (zoom > 90.0f) zoom = 90.0f;
+
+    switch (direction) {
+        case Camera_movement::zoom_in: {
+            zoom -= velocity;
+            break;
+        }
+        case Camera_movement::zoom_out: {
+            zoom += velocity;
+            break;
+        }
     }
 }
 
