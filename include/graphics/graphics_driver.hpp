40f932e07dd4cb81efa6ff2efc88580c08b8dfbb
GitHub Action <action@github.com>
2025-10-25 00:14:53 +0000

    Auto-format code

--- include/graphics/graphics_driver.hpp
+++ include/graphics/graphics_driver.hpp
@@ -5,11 +5,11 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
-#include <stdexcept>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <stdexcept>
 #include <unordered_set>
 #include <utility>
 #include <vector>
